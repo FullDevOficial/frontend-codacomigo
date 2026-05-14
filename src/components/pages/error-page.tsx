303f43f531c6b731b3ea91a9a3c5c5755f2bef98
Maria Clara Silva <maclara2018@gmail.com>
2026-05-14 19:24:02 -0300

    correção da página de erro

--- src/components/pages/error-page.tsx
+++ src/components/pages/error-page.tsx
@@ -4,31 +4,34 @@ import { Text } from '@/components/ds/text'
 
 export default function ErrorPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-200">
-      <div className="w-[393px] h-[852px] bg-white rounded-2xl shadow-md flex flex-col items-center justify-center px-6 text-center">
-        
-        <Logo size="sm" className="mb-1" />
+    <div className="w-full min-h-screen bg-white flex flex-col items-center justify-center px-6 text-center">
+      
+      <Logo size="sm" className="mb-1" />
 
-        <Heading size="xs" weight="bold" as="h1" className="text-[#FB433C] mb-1">
-          Seu PR quebrou a main...
-        </Heading>
+      <Heading
+        size="xs"
+        weight="bold"
+        as="h1"
+        className="text-[#FB433C] mb-1"
+      >
+        Seu PR quebrou a main...
+      </Heading>
 
-        <Text
-          size="p2"
-          tone="black"
-          weight="regular"
-          className="max-w-95 leading-none"
-        >
-          Não encontramos uma conta conectada ao{' '}
-          <Text size="p2" as="span" tone="brand" weight="regular">
-            Google
-          </Text>
-          .
-          <br />
-          Tente entrar com outra conta.
+      <Text
+        size="p2"
+        tone="black"
+        weight="regular"
+        className="leading-none"
+      >
+        Não encontramos uma conta conectada ao{' '}
+        <Text size="p2" as="span" tone="brand" weight="regular">
+          Google
         </Text>
+        .
+        <br />
+        Tente entrar com outra conta.
+      </Text>
 
-      </div>
     </div>
   )
 }
